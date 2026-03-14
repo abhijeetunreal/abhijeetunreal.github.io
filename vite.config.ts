bdd725243c4fd2abfb22c7199a541bfefa38da86
ABHIJEET <71241536+abhijeetunreal@users.noreply.github.com>
2026-03-14 21:41:59 +0530

    workflow added to publish on github

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths for static deployment
+  base: process.env.BASE_PATH || './',
   server: {
     port: 8080
   },
